b035d874e618281ca5248fddeb89b00d0edabbd9
julianll1202 <a20490712@itmexicali.edu.mx>
2022-08-24 20:01:59 -0700

    Added multiplication and division functions

--- calculator.js
+++ calculator.js
@@ -5,10 +5,23 @@ function calculate(){
     let num2 = Number(prompt("Please enter another number:"));
     console.log(num1, num2);
     sum(num1, num2);
+    sub(num1, num2);
+    mult(num1, num2);
+    div(num1, num2);
 }
-function sum(num1, num2){
-    document.getElementById("results").innerHTML= num1+num2;
+function sum(a, b){
+    document.getElementById("results").innerHTML += `<p>The result of the sum is ${a+b}</p>`;
 }
 
-function substract(a,b){}
+function sub(a,b){
+    document.getElementById("results").innerHTML += `<p>The result of the substraction is ${a-b}</p>`;
+}
 
+function mult(a, b){
+    document.getElementById("results").innerHTML += `<p>The result of the multiplication is ${a*b}</p>`;
+
+}
+
+function div(a, b){
+    document.getElementById("results").innerHTML += `<p>The result of the division is ${a/b}</p>`; 
+}
